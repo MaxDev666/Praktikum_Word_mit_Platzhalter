2873c09e4fa58a760937c0dc1df9a47f6bf3ca2b
MaxDev666 <36930595+MaxDev666@users.noreply.github.com>
2020-03-23 10:51:31 +0100

    Datenbankconnection; Dateien angelegt
    Dependency in pom.xml hinzugefügt
    Entity Vorlagenschrank angelegt
    Verbindungskonfiguration in persistence.xml

---
+++ src/main/java/jpa_entity/Vorlagenschrank.java
@@ -0,0 +1,39 @@
+package jpa_entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vorlagenschrank")
+public class Vorlagenschrank implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "id", unique = true)
+	private String id;
+	
+	@Column(name = "mainDokument", nullable = false)
+	private String dokumentinhalt;
+	
+    public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getWord() {
+		return dokumentinhalt;
+	}
+
+	public void setWord(String word) {
+		this.dokumentinhalt = word;
+	}
+    
+    
+}
